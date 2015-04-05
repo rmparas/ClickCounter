aaa39787578f575b736f8a302948325b7cf855ba
rmparas <rmparas@yahoo.com>
2015-04-05 10:18:37 -0700

    initial load

--- ClickCounter/ViewController.swift
+++ ClickCounter/ViewController.swift
@@ -10,16 +10,76 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var count = 0
+    var label:UILabel!
+    var label2:UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Label
+        var label = UILabel()
+        label.frame = CGRectMake(150, 150, 60, 60)
+        label.text = "0"
+        self.label = label
+        
+        self.view.addSubview(label)
+        
+        
+        //Label2
+        var label2 = UILabel()
+        label2.frame = CGRectMake(150, 50, 60, 60)
+        label2.text = "0"
+        self.label2 = label2
+        
+        self.view.addSubview(label2)
+
+        //Increment Button (150, 250, 60, 60)
+        var button = UIButton()
+        button.frame = CGRectMake(200, 250, 60, 60)
+        button.setTitle("ADD", forState: .Normal)
+        button.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        self.view.addSubview(button)
+        
+        button.addTarget(self, action: "incrementCount", forControlEvents: UIControlEvents.TouchUpInside)
+        
+        //Decrement Button
+        var buttonDecr = UIButton()
+        buttonDecr.frame = CGRectMake(50, 250, 60, 60)
+        buttonDecr.setTitle("MINUS", forState: .Normal)
+        buttonDecr.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        self.view.addSubview(buttonDecr)
+
+        buttonDecr.addTarget(self, action: "decrementCount", forControlEvents: UIControlEvents.TouchUpInside)
+        
+        //Change Color Button
+        var buttonColor = UIButton()
+        buttonColor.frame = CGRectMake(150, 350, 90, 60)
+        
+        buttonColor.setTitle("COLOR", forState: .Normal)
+        buttonColor.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        self.view.addSubview(buttonColor)
+        
+        buttonColor.addTarget(self, action: "toggleColor", forControlEvents: UIControlEvents.TouchUpInside)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func incrementCount() {
+        self.count++
+        self.label.text = "\(self.count)"
+        self.label2.text = "\(self.count)"
+    }
+    func decrementCount() {
+        
+        self.count--
+        self.label.text = "\(self.count)"
+        self.label2.text = "\(self.count)"
     }
 
-
+    func toggleColor() {
+        if self.view.backgroundColor == UIColor.lightGrayColor() {
+            self.view.backgroundColor = UIColor.whiteColor()
+        } else {
+            self.view.backgroundColor = UIColor.lightGrayColor()
+        }    }
 }
-
